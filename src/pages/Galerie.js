472df091f1f8bdc56b62298157032c0e0f3a3074
Mombabil <mombabil.7@gmail.com>
2022-09-03 14:00:58 +0200

    creation du create-react-app et ajout des bibliotheques sass, react-router-dom, et redux

---
+++ src/pages/Galerie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Galerie = () => {
+  return (
+    <div>
+      <h1>GALERIE</h1>
+    </div>
+  );
+};
+
+export default Galerie;
---
+++ src/pages/Tarifs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tarifs = () => {
+  return (
+    <div>
+      <h1>Tarifs</h1>
+    </div>
+  );
+};
+
+export default Tarifs;
